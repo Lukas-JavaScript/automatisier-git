ceff6b8616276debb5d3a832671e7176ae8ba091
Lukas-JavaScript <lukaswittek96@gmail.com>
2025-11-04 17:28:33 +0100

    maked it better

--- main.py
+++ main.py
@@ -3,5 +3,18 @@ from os import system
 push = input('Do you want to push it after every modifire?(y/n)(default:y): ').lower()
 if push == '' or push == 'y':
     push = True
-    
-system
+else:
+    push = False
+
+init = input('Do you want to init?(y/n)(default:y): ').lower()
+if init == '' or init == 'y':
+    system('git init')
+
+def main():
+    while True:
+        change = input("Whats your changes?: ")
+        system('git add .')
+        system(f'git commit -m "{change}"')
+        if push:
+            system('git push')
+main()
